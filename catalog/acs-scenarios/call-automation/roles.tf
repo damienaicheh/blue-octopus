e10d4b4806d3c69f4a49e093c0668e2624b18c4e
Damien Aicheh <damienaicheh@microsoft.com>
2026-05-18 15:24:27 +0000

    feat: Update to latest names for Foundry roles.

--- catalog/acs-scenarios/call-automation/roles.tf
+++ catalog/acs-scenarios/call-automation/roles.tf
@@ -1,6 +1,6 @@
-resource "azurerm_role_assignment" "azure_ai_user" {
+resource "azurerm_role_assignment" "foundry_user" {
   scope                = azapi_resource.ai_services.id
-  role_definition_name = "Azure AI User"
+  role_definition_name = "Foundry User"
   principal_id         = data.azurerm_client_config.current.object_id
 }
 
