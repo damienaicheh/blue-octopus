dc3acdc4e07022a2605ca32ecc942a23d2c7c565
Damien Aicheh <damienaicheh@microsoft.com>
2026-06-22 15:06:48 +0000

    chore: moving folders.

--- catalog/ai-scenarios/ai-basics/ai_search.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_search_service" "this" {
-  name                          = format("srch-%s", local.resource_suffix_kebabcase)
-  resource_group_name           = local.resource_group_name
-  location                      = local.resource_group_location
-  sku                           = "standard"
-  public_network_access_enabled = true
-  tags                          = local.tags
-}
--- catalog/ai-scenarios/ai-basics/roles.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_role_assignment" "cognitive_services_open_ai_user" {
-  for_each             = toset(local.open_ai_access_users)
-  scope                = azurerm_cognitive_account.open_ai_swc.id
-  role_definition_name = "Cognitive Services OpenAI User"
-  principal_id         = each.value
-}
-
- 
