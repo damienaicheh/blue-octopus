67c172420c8bc73a7668a849da88f9e0399abebd
Damien Aicheh <damienaicheh@microsoft.com>
2026-06-22 07:56:05 +0000

    chore: Clean unused code.

--- wip/multi-region/log.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_log_analytics_workspace" "this" {
-  name                = format("log-%s", local.resource_suffix_kebabcase)
-  location            = local.resource_group_location
-  resource_group_name = local.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-  tags                = local.tags
-}
--- wip/multi-region/modules/bastion/public_ip.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_public_ip" "bastion" {
-  name                = format("pip-bas-%s", var.resource_suffix)
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
